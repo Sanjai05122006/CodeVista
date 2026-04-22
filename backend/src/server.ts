7dd72b99e5832a20b9d96c20eaa5fcf98f7b61b0
Sanjai <sanjai2410498@ssn.edu.in>
2026-04-22 14:11:16 +0530

    Update:dependencies for typescript-javascript compilation and path-var from global-env store

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,6 @@
 import app from "./app";
-import dotenv from "dotenv";
-dotenv.config();
+import { env } from "./config/env";
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(env.PORT, () => {
+  console.log(`🚀 Server running on port ${env.PORT}`);
 });
