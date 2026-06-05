5de34dbae498b7f12a8b5388d1bf06197b8238a4
Sanjai <sanjai2410498@ssn.edu.in>
2026-06-05 18:52:32 +0530

    chore:added the dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./temp-redesign/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
